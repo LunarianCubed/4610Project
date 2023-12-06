551015f0de6c998c7d7d281bd9f69e387a14a185
Lunarian3 <xxdsheep@gmail.com>
2023-12-05 19:59:22 -0600

    fix search?

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
 import {Link} from "react-router-dom";
+import axios from 'axios';
+
+
 
 const SearchPage = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
+    // const handleSearch = async () => {
+    //     try {
+    //         const response = await fetch(`/search?q=${searchTerm}`);
+    //         if (response.ok) {
+    //             const data = await response.json();
+    //             setSearchResults(data.data);
+    //         } else {
+    //             throw new Error('Failed to fetch data');
+    //         }
+    //     } catch (error) {
+    //         console.error('Error:', error);
+    //     }
+    // };
+
+
+
     const handleSearch = async () => {
         try {
-            const response = await fetch(`/search?q=${searchTerm}`);
-            if (response.ok) {
-                const data = await response.json();
-                setSearchResults(data.data);
+            const response = await axios.get(`/search`, {
+                params: {
+                    q: searchTerm
+                }
+            });
+
+            if (response.status === 200) {
+                setSearchResults(response.data.data);
             } else {
                 throw new Error('Failed to fetch data');
             }
